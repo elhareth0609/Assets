1b248aa2eedf36095d13b924c9e5127775bfcc18
elhareth0609 <elhareh0609@gmail.com>
2025-08-13 09:21:17 +0100

    ed

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -15,12 +15,13 @@
             <!-- Logo and Title -->
             <a href="#" class="mb-8 px-1 flex items-center sidebar-item-container">
                 <!-- Asset Management Icon -->
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-archive h-7 w-7 text-primary-800 dark:text-primary-400 flex-shrink-0">
+                <img src="{{ asset('assets/img/icon.png') }}" alt="Asset Management Icon" class="w-7 h-7 text-[#0783a1] dark:text-[#0783a1] flex-shrink-0">
+                {{-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-archive h-7 w-7 text-primary-800 dark:text-primary-400 flex-shrink-0">
                     <rect x="2" y="3" width="20" height="5" rx="1"/>
                     <path d="M4 8v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8"/>
                     <path d="M10 12h4"/>
-                </svg>
-                <span class="sidebar-text text-xl font-display font-semibold text-primary-800 dark:text-primary-400 rtl:ms-2">إدارة الأصول</span>
+                </svg> --}}
+                <span class="sidebar-text text-xl font-display font-semibold text-[#0783a1] dark:text-[#0783a1] rtl:ms-2">إدارة الأصول</span>
             </a>
 
             <!-- Main Navigation -->
