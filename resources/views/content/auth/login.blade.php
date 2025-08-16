ebc96dfa384569c7b7b51a5f5936c44d75b749a3
elhareth0609 <elhareh0609@gmail.com>
2025-08-16 22:14:30 +0100

    update login input type from email to text

--- resources/views/content/auth/login.blade.php
+++ resources/views/content/auth/login.blade.php
@@ -37,7 +37,7 @@
                         <span class="absolute inset-y-0 rtl:right-0 ltr:left-0 flex items-center rtl:pr-3 ltr:pl-3">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user w-5 h-5 text-slate-400"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
                         </span>
-                        <input id="email" type="email" name="email" :value="old('email')" required autofocus
+                        <input id="email" type="text" name="email" :value="old('email')" required autofocus
                             class="flex h-10 w-full rounded-md border border-slate-300 bg-white rtl:pr-10 ltr:pl-10 px-3 py-2 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-primary-400 focus:border-transparent disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-700 dark:bg-slate-900 dark:text-slate-50"
                             placeholder="ادخل اسم المستخدم">
                     </div>
